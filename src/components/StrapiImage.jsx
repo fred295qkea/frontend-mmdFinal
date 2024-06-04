881129b9bfbaabd9d8931a4b719e6090743aa292
”Frederik <fred295q@stud.kea.dk>
2024-06-04 14:51:23 +0200

    sizes...

--- src/components/StrapiImage.jsx
+++ src/components/StrapiImage.jsx
@@ -1,35 +1,16 @@
 import Image from "next/image";
 import { getStrapiMedia } from "@/lib/utils";
 
-
-export function StrapiImage({
-  src,
-  alt,
-  height,
-  width,
-  className,
-  priority,
-  objectFit,
-  sizes
-  
-}) {
+export function StrapiImage({ src, alt, height, width, className, priority, objectFit, sizes }) {
   //if (!src) return null;
   const imageUrl = getStrapiMedia(src);
 
   const imageFallback = "/t-shirts.jpg";
-  const altFallback = "Trøjer i forskellige farver som hænger på et stativ" 
-    const fitValue = objectFit || "cover";
+  const altFallback = "Trøjer i forskellige farver som hænger på et stativ";
+  const fitValue = objectFit || "cover";
   return (
-    <div style={{position:'relative', width:width, height:height}}>
-        <Image
-        style={{objectFit:fitValue}}
-        priority={priority || false}
-        src={imageUrl || imageFallback}
-        alt={alt || altFallback}
-        fill
-        className={className}
-        sizes={sizes}
-        />
+    <div style={{ position: "relative", width: width, height: height }}>
+      <Image style={{ objectFit: fitValue }} priority={priority || false} src={imageUrl || imageFallback} alt={alt || altFallback} fill className={className} sizes="50vw" />
     </div>
   );
 }
